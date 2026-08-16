4dea5b3f592ffe7367ef5b8d2efdaf19263756aa
lichking21 <kazakovaman@gmail.com>
2026-08-16 23:10:18 +0600

    (upd) functions rename

--- include/astring.h
+++ include/astring.h
@@ -2,7 +2,7 @@
 #define ASTRING_H
 #include <stddef.h>
 
-typedef struct 
+typedef struct
 {
     char* data;
     size_t size;
@@ -11,39 +11,32 @@ typedef struct
 }string;
 
 // ========== Memory management
-string* strnew(const char*);
-void strfree(string*);
+string* astr_new(const char*);
+void astr_free(string*);
 
 // ========== Capacity & Size
-size_t length(string*);
-void resize(string*, size_t);
-
-void append(string*, const string*);
-string* insert(string*, const char*, size_t pos);
-
-void pushback(string*, char);
-void popback(string*);
-
-void clear(string*);
-int empty(const string*);
-string* erase(string*, size_t pos, size_t len);
+size_t astr_length(string*);
+int astr_resize(string*, size_t);
+int astr_append(string*, const string*);
+string* astr_insert(string*, const char*, size_t pos);
+int astr_push_back(string*, char);
+void astr_pop_back(string*);
+void astr_clear(string*);
+int astr_empty(const string*);
+string* astr_erase(string*, size_t pos, size_t len);
 
 // ========== Elements accesess
-const char* at(const string*, size_t);
-
-const char* back(const string*);
-const char* begin(const string*);
-const char* end(const string*);
-
-size_t copy(string*, char*, size_t, size_t);
-int compareall(const string*, const string*);
-int compare(string*, size_t, size_t, string*);
-
-size_t find(const string*, const string*);
-size_t rfind(const string*, const string*);
-string* substr(const string*, size_t, size_t);
-
-string* replace(string*, size_t, size_t, const string*);
-void swap(string*, string*);
+const char* astr_at(const string*, size_t);
+const char* astr_back(const string*);
+const char* astr_begin(const string*);
+const char* astr_end(const string*);
+size_t astr_copy(string*, char*, size_t, size_t);
+int astr_comparea_ll(const string*, const string*);
+int astr_compare(string*, size_t, size_t, string*);
+size_t astr_find(const string*, const string*);
+size_t astr_rfind(const string*, const string*);
+string* astr_substr(const string*, size_t, size_t);
+string* astr_replace(string*, size_t, size_t, const string*);
+void astr_swap(string*, string*);
 
 #endif
