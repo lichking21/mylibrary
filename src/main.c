0c1dd60e84a59aaa166a466ca1a0a0f4f72e4e2f
lichking21 <kazakovaman@gmail.com>
2026-08-17 20:13:28 +0600

    main upd

--- src/main.c
+++ src/main.c
@@ -8,46 +8,46 @@ int main() {
     size_t elem_size = sizeof(char*);
     size_t capacity = 8;
 
-    vector* vec = vecnew(elem_size, capacity);
+    vector* vec = avec_new(elem_size, capacity);
 
     char* c = "FRIENDLY";
     char* c2 = "THUGG";
     char* c3 = "52";
 
-    vpush_back(vec, &c);
-    vpush_back(vec, &c2);
-    vpush_back(vec, &c3);
+    avec_push_back(vec, &c);
+    avec_push_back(vec, &c2);
+    avec_push_back(vec, &c3);
 
-    char** ngg = (char**)vdata(vec);
+    char** ngg = (char**)avec_data(vec);
 
     printf("Vec1 content: ");
     for (size_t i = 0; i < vec->size; i++) printf("%s ", ngg[i]);
     printf("\n");
 
-    vector* vec2 = vecnew(sizeof(char*), 8);
+    vector* vec2 = avec_new(sizeof(char*), 8);
     char* cc = "TUN";
     char* cc2 = "TUN";
     char* cc3 = "SAHUR";
 
-    vpush_back(vec2, &cc);
-    vpush_back(vec2, &cc2);
-    vpush_back(vec2, &cc3);
+    avec_push_back(vec2, &cc);
+    avec_push_back(vec2, &cc2);
+    avec_push_back(vec2, &cc3);
 
-    char** tts = (char**)vdata(vec2);
+    char** tts = (char**)avec_data(vec2);
 
     printf("Vec2 content: ");
     for (size_t i = 0; i < vec2->size; i++) printf("%s ", tts[i]);
     printf("\n");
 
     // New functions use example
-    vswap(vec, vec2);
-    char** newvec = (char**)vdata(vec);
+    avec_swap(vec, vec2);
+    char** newvec = (char**)avec_data(vec);
 
     printf("Vec1 content: ");
     for (size_t i = 0; i < vec->size; i++) printf("%s ", newvec[i]);
     printf("\n");
 
-    char** newvec2 = (char**)vdata(vec2);
+    char** newvec2 = (char**)avec_data(vec2);
     printf("Vec2 content: ");
     for (size_t i = 0; i < vec2->size; i++) printf("%s ", newvec2[i]);
     printf("\n");
